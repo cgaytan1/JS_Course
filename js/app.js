219b5ee16ff0203514b91a507531c47b3bb36443
cgaytan1 <cesar.gaytanv@gmail.com>
2021-05-28 18:43:50 -0500

    Using symbols in JavaScript

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 "use strict"
 
-var fecha = new Date();
+// Los valores simbol son unicos en sí, aunque parezca que no.
+var simbolo1 = Symbol();
+var simbolo2 = Symbol();
 
-/*
-fecha.getDay() -> Día de la semana
-fecha.getDate() -> Día del mes
-fecha.getHours() -> La hora especifica
-Etc, basta con poner fecha.get... apareceran todas las opciones disponibles.
-Cambiar el GET por el SET nos permitira personalizar los datos que queremos que se muestren según nuestra necesidad.
-*/
+var ambiente = Symbol('dev');
